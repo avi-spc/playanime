618f618db07360d05e9bf2c5cda99594a74ed8ba
Avdhesh Yadav <avdhesh7394vn@gmail.com>
2023-04-15 03:23:34 +0530

    feat: created featured-anime component :sparkles:

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import AlphanumericSearch from "./components/AlphanumericSearch";
 import AnimeDetails from "./components/AnimeDetails";
 import AnimeGrid from "./components/AnimeGrid";
 import AnimeList from "./components/AnimeList";
+import FeaturedAnime from "./components/FeaturedAnime";
 import Navbar from "./components/Navbar";
 import CharactersContextProvider from "./contexts/charactersContext";
 
@@ -9,7 +10,7 @@ const App = () => {
   return (
     <CharactersContextProvider>
       <div className="App">
-        <Navbar />
+        <FeaturedAnime />
       </div>
     </CharactersContextProvider>
   );
---
+++ src/components/FeaturedAnime.js
@@ -0,0 +1,54 @@
+const FeaturedAnime = () => {
+  return (
+    <div className="featured-anime">
+      <img
+        className="featured-anime__image"
+        src="./images/kiminonawa.jpg"
+        alt=""
+      />
+      <div className="anime-details">
+        <div className="anime-details__info">
+          <h2>Skip and Loafer</h2>
+          <p className="anime-details__info__synopsis">
+            The second season of Mahoutsukai no Yome. Chise was able to accept
+            Elias and herself, if not necessarily everything about her
+            situation. After Cartaphilus fell back into a slumber that would not
+            last forever, Chise was able to go back to her regular life. Then
+            she receives an invitation from a mutual aid organization for mages
+            called the College.
+          </p>
+          <div className="anime-details__info__meta">
+            <div>
+              <div className="meta-type">Studio</div>
+              <div className="meta-value">CygamesPictures</div>
+            </div>
+            <div>
+              <div className="meta-type">Episodes</div>
+              <div className="meta-value">12</div>
+            </div>
+            <div>
+              <div className="meta-type">Genre</div>
+              <div className="meta-value">Comedy</div>
+            </div>
+          </div>
+          <div className="anime-details__cta">
+            <button className="btn btn--watch">
+              <img src="./images/icons/play.png" alt="" />
+              <h3>Watch</h3>
+            </button>
+            <div className="prev-p-next">
+              <button className="btn btn--trans">
+                <img src="./images/icons/arrow_left.png" alt="" />
+              </button>
+              <button className="btn btn--trans">
+                <img src="./images/icons/arrow_right.png" alt="" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedAnime;
