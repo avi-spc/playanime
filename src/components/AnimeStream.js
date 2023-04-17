b7e4ac961de29114854baa3379410eb724010817
Avdhesh Yadav <avdhesh7394vn@gmail.com>
2023-04-17 12:26:51 +0530

    feat, style: inserted iframe in anime-stream component for streaming content from referer url :sparkles: :lipstick:

--- src/components/AnimeStream.js
+++ src/components/AnimeStream.js
@@ -4,7 +4,12 @@ import EpisodesCount from "./EpisodesCount";
 const AnimeStream = () => {
   return (
     <section className="anime-stream main-content">
-      <img src="./images/kiminonawa.jpg" alt="" />
+      <iframe
+        src="https://anihdplay.com/streaming.php?id=OTk0NTM=&title=Made+in+Abyss+Episode+2&typesub=SUB"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+      ></iframe>
       <EpisodesCount />
       <AnimeDetails />
     </section>
