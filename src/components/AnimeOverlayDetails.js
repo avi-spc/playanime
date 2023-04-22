692f1bc1c38fccf28fe7060b58b5b92cb26efd5b
Avdhesh Yadav <avdhesh7394vn@gmail.com>
2023-04-22 20:47:44 +0530

    feat: displayed overlay foreground on image while hovering anime-tile :sparkles:

--- src/components/AnimeOverlayDetails.js
+++ src/components/AnimeOverlayDetails.js
@@ -2,20 +2,14 @@ const AnimeOverlayDetails = ({ details }) => {
   return (
     <div className="anime-overlay-details">
       <h4>{details.animeTitle}</h4>
-      <p className="anime-overlay-details__synopsis">
-        The second season of Mahoutsukai no Yome. Chise was able to accept Elias
-        and herself, if not necessarily everything about her situation. After
-        Cartaphilus fell back into a slumber that would not last forever, Chise
-        was able to go back to her regular life. Then she receives an invitation
-        from a mutual aid organization for mages called the College.
-      </p>
+      <p className="anime-overlay-details__synopsis">{details.synopsis}</p>
       <div className="anime-overlay-details__meta">
         <div className="meta-type">Episodes</div>
         <div className="meta-value">{details.totalEpisodes}</div>
         <div className="meta-type">Genre</div>
         <div className="meta-value">{details.genres.join(", ")}</div>
         <div className="meta-type">Date Aired</div>
-        <div className="meta-value">Apr 6, 2023</div>
+        <div className="meta-value">{details.releasedDate}</div>
       </div>
       <button className="btn btn--lg">
         <h4>Watch</h4>
--- src/components/AnimeTile.js
+++ src/components/AnimeTile.js
@@ -8,7 +8,12 @@ const AnimeTile = ({ anime, details }) => {
   return (
     <NavLink to={`/stream/${anime.animeId}`}>
       <div className="anime-tile">
-        <img className="anime-tile__image" src={anime.animeImg} alt="" />
+        <div className="anime-tile__img-p-overlay">
+          <img className="anime-tile__image" src={anime.animeImg} alt="" />
+          <div className="anime-tile__hover-overlay">
+            <img src="/images/icons/play.png" alt="" />
+          </div>
+        </div>
         <div className="anime-tile__name">
           {anime.animeTitle !== ""
             ? anime.animeTitle
